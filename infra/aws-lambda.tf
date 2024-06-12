98b153adb6df890e7ee5486fe5ecbc8957d41ed9
ssheraliev <ssheraliev@outlook.com>
2024-06-12 04:48:23 -0700

    attempt to use EKS resource

--- infra/aws-lambda.tf
+++ infra/aws-lambda.tf
@@ -1,30 +1,14 @@
-resource "aws_lambda_function" "service_provisioner" {
-  # Path to combined Lambda function
-  filename = "../lambda-func/lambda_handler.py"
-  # Handler function within the code
-  handler  = "lambda_handler.lambda_handler"
-
-  # Function name for Lambda function
-  function_name = "<lambda-function-name>"
-
-  # Environment variables for the Lambda function
-  environment {
-    variables = {
-      # Diagrid Catalyst region
-      "DIAGRID_REGION" = "us-west-1"
-    }
-  }
-
-  # IAM role for the Lambda function
-  role = aws_iam_role.lambda_role.arn
+# AWS IAM role and policies required
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "../lambda-func/lambda_handler.py"
+  output_path = "lambda-handler.zip"
 }
 
-# Resource to define the IAM role for the Lambda function
-resource "aws_iam_role" "lambda_role" {
-  # Name for the IAM role
-  name = "lambda_provisioner_role"
+# IAM Role for EKS Provisioner
+resource "aws_iam_role" "eks_provisioner_role" {
+  name = "eks_provisioner_role"
 
-  # Assume the role
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -39,39 +23,155 @@ resource "aws_iam_role" "lambda_role" {
   ]
 }
 EOF
-
-  # attaching IAM policy document(s)
-  # ... (add policy documents)
 }
 
-# Resource to define the IAM policy for the Lambda function
-resource "aws_iam_policy" "lambda_provisioner_policy" {
-  # Name for the IAM policy
-  name = "lambda_provisioner_policy"
+resource "aws_iam_policy" "eks_provisioner_policy" {
+  name = "eks_provisioner_policy"
 
-  # Policy document with permissions for the Lambda function
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        # Allow Diagrid Catalyst API calls
-        "Effect": "Allow",
-        "Action": [
-          "diagrid:StartWorkflow"
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateCluster",
+          "ec2:DescribeCluster",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "eks:CreateCluster",
+          "eks:DescribeCluster",
+          "iam:CreateServiceRole",
+          "iam:PassRole",
+          "iam:AttachRolePolicy",
+          "iam:ListAttachedRolePolicies",
+          "iam:DeleteRolePolicy",
+          "iam:CreateRole",
+          "iam:GetRole",
+          "iam:DeleteRole",
+          "iam:ListRoles",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "securitygroup:CreateSecurityGroup",
+          "securitygroup:DeleteSecurityGroup",
+          "securitygroup:DescribeSecurityGroups",
+          "securitygroup:AuthorizeSecurityGroupIngress",
+          "securitygroup:RevokeSecurityGroupIngress",
+          "subnet:CreateSubnet",
+          "subnet:DescribeSubnets",
+          "vpc:CreateVpc",
+          "vpc:DescribeVpcs",
+          "sns:Publish"
         ],
-        # Allow access to specific Diagrid resources
-        "Resource": [
-          "arn:aws:diagrid:<region>:<account-id>:apps/<app-id>/workflows/*"
-        ]
-      },
+        "Resource" : "*"
+      }
     ]
   })
 }
 
-# Resource to attach the IAM policy to the IAM role
-resource "aws_iam_role_attachment" "lambda_role_attachment" {
-  # Reference to the IAM role
-  role       = aws_iam_role.lambda_role.arn
-  # Reference to the IAM policy
-  policy_arn = aws_iam_policy.lambda_provisioner_policy.arn
+resource "aws_iam_role_policy_attachment" "eks_role_attachment" {
+  role       = aws_iam_role.eks_provisioner_role.name
+  policy_arn = aws_iam_policy.eks_provisioner_policy.arn
+}
+
+# IAM Role for RDS Provisioner
+resource "aws_iam_role" "rds_provisioner_role" {
+  name = "rds_provisioner_role"
+
+  assume_role_policy = aws_iam_role.eks_provisioner_role.assume_role_policy
+}
+
+resource "aws_iam_policy" "rds_provisioner_policy" {
+  name = "rds_provisioner_policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "rds:CreateDBInstance",
+          "rds:DescribeDBInstances",
+          "iam:PassRole",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rds_role_attachment" {
+  role       = aws_iam_role.rds_provisioner_role.name
+  policy_arn = aws_iam_policy.rds_provisioner_policy.arn
+}
+
+# IAM Role for ElastiCache Provisioner
+
+resource "aws_iam_role" "elasticache_provisioner_role" {
+  name = "elasticache_provisioner_role"
+
+  assume_role_policy = aws_iam_role.eks_provisioner_role.assume_role_policy
+}
+
+resource "aws_iam_policy" "elasticache_provisioner_policy" {
+  name = "elasticache_provisioner_policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "elasticache:CreateCacheCluster",
+          "elasticache:DescribeCacheClusters",
+          "iam:PassRole",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "elasticache_role_attachment" {
+  role       = aws_iam_role.elasticache_provisioner_role.name
+  policy_arn = aws_iam_policy.elasticache_provisioner_policy.arn
+}
+
+# Lambda Functions with Separate Roles
+
+resource "aws_lambda_function" "eks_provisioner" {
+  function_name = "eks-provisioner"
+  handler       = "lambda_handler.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.python_lambda_package.output_path
+  role          = aws_iam_role.eks_provisioner_role.arn
+
+  depends_on = [aws_iam_role_policy_attachment.eks_role_attachment]
+}
+
+resource "aws_lambda_function" "rds_provisioner" {
+  function_name = "rds-provisioner"
+  handler       = "lambda_handler.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.python_lambda_package.output_path
+  role          = aws_iam_role.rds_provisioner_role.arn
+
+  depends_on = [aws_iam_role_policy_attachment.rds_role_attachment]
+}
+
+resource "aws_lambda_function" "elasticache_provisioner" {
+  function_name = "elasticache-provisioner"
+  handler       = "lambda_handler.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.python_lambda_package.output_path
+  role          = aws_iam_role.elasticache_provisioner_role.arn
+
+  depends_on = [aws_iam_role_policy_attachment.elasticache_role_attachment]
 }
